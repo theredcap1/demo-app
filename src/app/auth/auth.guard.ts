d6954fd939d9493a83a08836b16cf32d44046a0a
Brahim Redouane LEBAD <redouaneleb@gmail.com>
2025-03-27 12:06:39 +0100

    feat(sessions): Add JWT-based session management through localStorage
    Took 6 minutes
    
    Took 3 minutes

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,5 +1,4 @@
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
-import {AuthService} from "./auth.service";
 import {inject, Injectable} from "@angular/core";
 import {SessionManagementService} from "./session-management.service";
 
@@ -11,8 +10,6 @@ export class AuthGuard implements CanActivate {
   constructor(private router : Router) {}
   private session : SessionManagementService = inject(SessionManagementService);
   canActivate(route : ActivatedRouteSnapshot, state : RouterStateSnapshot) : boolean {
-    console.log("we here");
-    console.log(this.session.isAuthenticated());
     if (this.session.isAuthenticated()) return true;
     this.router.navigate(['/auth/login']);
     return false;
